7b4df5a1c6cb13ba590af5a1ec555f23ae12f4c6
Ganzidaeyong <iwin1203@naver.com>
2024-11-20 23:09:12 +0900

    fix typo

--- core/src/main/kotlin/siksha/wafflestudio/core/domain/common/exception/ExceptionHandler.kt
+++ core/src/main/kotlin/siksha/wafflestudio/core/domain/common/exception/ExceptionHandler.kt
@@ -1,13 +1,13 @@
 package siksha.wafflestudio.core.domain.common.exception
 
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.RestControllerAdvice
 import org.springframework.web.bind.annotation.ExceptionHandler
+import org.springframework.web.bind.annotation.RestControllerAdvice
 
 @RestControllerAdvice
 class ExceptionHandler {
     @ExceptionHandler(SikshaException::class)
-    fun handleEduspaceException(sikshaException: SikshaException): ResponseEntity<ErrorBody> {
+    fun handleSikshaException(sikshaException: SikshaException): ResponseEntity<ErrorBody> {
         return ResponseEntity(
             ErrorBody(sikshaException.errorMessage),
             sikshaException.httpStatus,
