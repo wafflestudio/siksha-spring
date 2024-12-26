89a2641589e2291583653ffa8477eb7c2fd26ea3
정지민 <jungjimin424@naver.com>
2024-12-26 16:00:16 +0900

    add post jpa entities

---
+++ core/src/main/kotlin/siksha/wafflestudio/core/domain/post/data/Post.kt
@@ -0,0 +1,32 @@
+package siksha.wafflestudio.core.domain.post.data
+
+import jakarta.persistence.*
+import siksha.wafflestudio.core.domain.user.data.User
+import java.time.LocalDateTime
+
+@Entity(name = "post")
+@Table(name = "post")
+class Post(
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    val id: Long = 0L,
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    val user: User,
+
+//    FIXME: Board entity 생성 후 연결
+//    @ManyToOne(fetch = FetchType.LAZY)
+//    @JoinColumn(name = "board_id", nullable = false)
+//    val board: Board,
+
+    @Column(name = "title", length = 200)
+    val title: String,
+
+    val content: String,
+    val available: Boolean? = null,
+    val anonymous: Boolean,
+    val createdAt: LocalDateTime,
+    val updatedAt: LocalDateTime,
+    val etc: String? = null,
+)
