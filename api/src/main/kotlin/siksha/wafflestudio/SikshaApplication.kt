56fdd6845d67105a6e10a31420424fb4ed17286a
soobin-jeon <nname@snu.ac.kr>
2025-03-01 17:23:37 +0900

    timezone 설정

--- api/src/main/kotlin/siksha/wafflestudio/SikshaApplication.kt
+++ api/src/main/kotlin/siksha/wafflestudio/SikshaApplication.kt
@@ -1,10 +1,17 @@
 package siksha.wafflestudio
 
+import jakarta.annotation.PostConstruct
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import java.util.TimeZone
 
 @SpringBootApplication
-class SikshaApplication
+class SikshaApplication {
+    @PostConstruct
+    fun init() {
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"))
+    }
+}
 
 fun main() {
     runApplication<SikshaApplication>()
