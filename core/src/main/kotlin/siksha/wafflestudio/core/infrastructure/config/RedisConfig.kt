ff4df57a321db1580226960b211150642ba43c3b
Jimin Jung <jungjimin424@naver.com>
2025-04-20 19:15:57 +0900

    feat: AWS secrets manager 사용 (#31)
    * feat: SecretsManager에서 dev secrets 가져옴
    
    * fix: page number 오류 시 500 대신 422
    
    * fix dev redis env var
    
    * fix local profile redis
    
    * remove unnecessary dependencies

--- core/src/main/kotlin/siksha/wafflestudio/core/infrastructure/config/RedisConfig.kt
+++ core/src/main/kotlin/siksha/wafflestudio/core/infrastructure/config/RedisConfig.kt
@@ -1,8 +1,10 @@
 package siksha.wafflestudio.core.infrastructure.config
 
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.core.env.Environment
 import org.springframework.data.redis.connection.RedisConnectionFactory
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory
 import org.springframework.data.redis.core.RedisTemplate
@@ -11,13 +13,16 @@ import org.springframework.data.redis.serializer.StringRedisSerializer
 
 @Configuration
 class RedisConfig(
-    @Value("\${spring.redis.host}")
+    @Value("\${spring.data.redis.host}")
     val host: String,
-    @Value("\${spring.redis.port}")
+    @Value("\${spring.data.redis.port}")
     val port: Int,
+    @Autowired
+    val env: Environment,
 ) {
     @Bean
     fun redisConnectionFactory(): RedisConnectionFactory {
+        if (env.activeProfiles.contains("local")) return LettuceConnectionFactory("localhost", port)
         return LettuceConnectionFactory(host, port)
     }
 
