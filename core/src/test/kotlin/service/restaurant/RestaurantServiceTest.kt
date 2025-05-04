69ea9c6a6b2ace6e3c3750f6a2c0f3ec0177ef94
soobin-jeon <nname@snu.ac.kr>
2025-05-04 21:09:32 +0900

    테스트 추가

---
+++ core/src/test/kotlin/service/restaurant/RestaurantServiceTest.kt
@@ -0,0 +1,48 @@
+package siksha.wafflestudio.core.service.restaurant
+
+import io.mockk.clearAllMocks
+import io.mockk.every
+import io.mockk.mockk
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import siksha.wafflestudio.core.domain.restaurant.data.Restaurant
+import siksha.wafflestudio.core.domain.restaurant.repository.RestaurantRepository
+import siksha.wafflestudio.core.domain.restaurant.service.RestaurantService
+import kotlin.test.assertNotNull
+
+class RestaurantServiceTest {
+    private lateinit var restaurantRepository: RestaurantRepository
+    private lateinit var service: RestaurantService
+
+    @BeforeEach
+    internal fun setUp() {
+        restaurantRepository = mockk()
+        service = RestaurantService(restaurantRepository)
+        clearAllMocks()
+    }
+
+    @Test
+    fun `get restaurants`() {
+        // given
+        val board = Restaurant(
+            id = 1,
+            code = "test",
+            nameKr = "test",
+            nameEn = "test",
+            addr = "test",
+            lat = 0.0,
+            lng = 0.0,
+            etc = null,
+        )
+        every { restaurantRepository.findAll() } returns listOf(board)
+
+        // when
+        val result = service.getAllRestaurants()
+
+        // when
+        assertNotNull(result)
+        assertEquals(result.result.size, result.count)
+        assertEquals(result.result[0].id, 1)
+    }
+}
