5b06399fa7f3e0f216da51e3a4d275cc8083d056
soobin-jeon <nname@snu.ac.kr>
2025-05-04 15:51:44 +0900

    response 수정

--- api/src/main/kotlin/siksha/wafflestudio/api/controller/RestaurantController.kt
+++ api/src/main/kotlin/siksha/wafflestudio/api/controller/RestaurantController.kt
@@ -1,10 +1,8 @@
 package siksha.wafflestudio.api.controller
 
-import jakarta.servlet.http.HttpServletRequest
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
-import siksha.wafflestudio.api.common.userId
-import siksha.wafflestudio.core.domain.restaurant.data.Restaurant
+import siksha.wafflestudio.core.domain.restaurant.dto.RestaurantListResponseDto
 import siksha.wafflestudio.core.domain.restaurant.service.RestaurantService
 
 @RestController
@@ -12,7 +10,7 @@ class RestaurantController(
     private val restaurantService: RestaurantService,
 ) {
     @GetMapping("/restaurants")
-    fun getRestaurants(request: HttpServletRequest): List<Restaurant> {
-        return restaurantService.getRestaurants(request.userId)
+    fun getRestaurants(): RestaurantListResponseDto {
+        return restaurantService.getAllRestaurants()
     }
 }
--- core/src/main/kotlin/siksha/wafflestudio/core/domain/restaurant/service/RestaurantService.kt
+++ core/src/main/kotlin/siksha/wafflestudio/core/domain/restaurant/service/RestaurantService.kt
@@ -1,14 +1,21 @@
 package siksha.wafflestudio.core.domain.restaurant.service
 
 import org.springframework.stereotype.Service
-import siksha.wafflestudio.core.domain.restaurant.data.Restaurant
+import siksha.wafflestudio.core.domain.restaurant.dto.RestaurantResponseDto
+import siksha.wafflestudio.core.domain.restaurant.dto.RestaurantListResponseDto
 import siksha.wafflestudio.core.domain.restaurant.repository.RestaurantRepository
 
 @Service
 class RestaurantService(
     private val restaurantRepository: RestaurantRepository,
 ) {
-    fun getRestaurants(userId: Int): List<Restaurant> {
-        return restaurantRepository.findAll()
+    fun getAllRestaurants(): RestaurantListResponseDto {
+        val restaurants = restaurantRepository.findAll()
+        return RestaurantListResponseDto(
+            count = restaurants.size,
+            result = restaurants.map { restaurant->
+                RestaurantResponseDto.from(restaurant)
+            }
+        )
     }
 }
