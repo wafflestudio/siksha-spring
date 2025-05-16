cee2c3d74f0648f69baf2241f574a9a3c3ac56a5
곽민지 <beinte0419@snu.ac.kr>
2025-05-16 17:19:17 +0900

    feat: jpa projection 연결용 alias 정의

--- core/src/main/kotlin/siksha/wafflestudio/core/domain/main/menu/repository/MenuRepository.kt
+++ core/src/main/kotlin/siksha/wafflestudio/core/domain/main/menu/repository/MenuRepository.kt
@@ -10,11 +10,11 @@ import siksha.wafflestudio.core.domain.main.menu.dto.MenuSummary
 
 interface MenuRepository : JpaRepository<Menu, Int> {
     @Query("""
-        SELECT m.id, m.restaurant_id, m.code, m.date, m.type, m.name_kr, m.name_en, m.price, m.etc,
-            CONVERT_TZ(m.created_at, '+00:00', '+09:00') AS created_at,
-            CONVERT_TZ(m.updated_at, '+00:00', '+09:00') AS updated_at,
+        SELECT m.id, m.restaurant_id AS restaurantId, m.code, m.date, m.type, m.name_kr AS nameKr, m.name_en AS nameEn, m.price, m.etc,
+            CONVERT_TZ(m.created_at, '+00:00', '+09:00') AS createdAt,
+            CONVERT_TZ(m.updated_at, '+00:00', '+09:00') AS updatedAt,
             agg.score AS score,
-            IFNULL(agg.review_cnt, 0) AS review_cnt
+            IFNULL(agg.review_cnt, 0) AS reviewCnt
         FROM menu m
             LEFT JOIN (
                 SELECT me.restaurant_id, me.code, AVG(r.score) AS score, COUNT(r.id) AS review_cnt
@@ -27,7 +27,7 @@ interface MenuRepository : JpaRepository<Menu, Int> {
 
     @Query(
         """
-        SELECT m.id, COUNT(ml.id) AS like_cnt, MAX(IF(ml.user_id = :user_id, 1, 0)) AS is_liked
+        SELECT m.id, COUNT(ml.id) AS likeCnt, MAX(IF(ml.user_id = :user_id, 1, 0)) AS isLiked
         FROM menu m
         JOIN menu me ON m.restaurant_id = me.restaurant_id AND m.code = me.code
         LEFT OUTER JOIN siksha.menu_like ml ON me.id = ml.menu_id AND ml.is_liked = 1
@@ -38,11 +38,11 @@ interface MenuRepository : JpaRepository<Menu, Int> {
 
     @Query(
         """
-        SELECT m.id, m.restaurant_id, m.code, m.date, m.type, m.name_kr, m.name_en, m.price, m.etc,
-            CONVERT_TZ(m.created_at, '+00:00', '+09:00') AS created_at,
-            CONVERT_TZ(m.updated_at, '+00:00', '+09:00') AS updated_at,
+        SELECT m.id, m.restaurant_id AS restaurantId, m.code, m.date, m.type, m.name_kr AS nameKr, m.name_en AS nameEn, m.price, m.etc,
+            CONVERT_TZ(m.created_at, '+00:00', '+09:00') AS createdAt,
+            CONVERT_TZ(m.updated_at, '+00:00', '+09:00') AS updatedAt,
             agg.score AS score,
-            IFNULL(agg.review_cnt, 0) AS review_cnt
+            IFNULL(agg.review_cnt, 0) AS reviewCnt
         FROM menu m
             LEFT JOIN (
                 SELECT me.restaurant_id, me.code, AVG(r.score) AS score, COUNT(r.id) AS review_cnt
@@ -55,7 +55,7 @@ interface MenuRepository : JpaRepository<Menu, Int> {
     fun findMenuById(@Param("menu_id") menuId: String): MenuSummary
 
     @Query("""
-        SELECT m.id, agg.like_cnt AS like_cnt
+        SELECT m.id, agg.like_cnt AS likeCnt
         FROM menu m
         LEFT JOIN (
             SELECT m.restaurant_id, m.code, COUNT(ml.id) as like_cnt
@@ -68,7 +68,7 @@ interface MenuRepository : JpaRepository<Menu, Int> {
     fun findMenuLikeByMenuId(@Param("menu_id") menuId: String): MenuLikeCount
 
     @Query("""
-        SELECT m.id, COUNT(ml.id) AS like_cnt, MAX(IF(ml.user_id = :user_id, 1, 0)) AS is_liked
+        SELECT m.id, COUNT(ml.id) AS likeCnt, MAX(IF(ml.user_id = :user_id, 1, 0)) AS isLiked
         FROM menu m
         JOIN menu me ON m.restaurant_id = me.restaurant_id AND m.code = me.code
         LEFT OUTER JOIN menu_like  ml ON me.id = ml.menu_id AND ml.is_liked = 1
