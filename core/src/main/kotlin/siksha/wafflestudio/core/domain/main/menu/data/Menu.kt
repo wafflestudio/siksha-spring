07de25f3220cf38c13dfabe4d6123602b62d5cc9
곽민지 <beinte0419@snu.ac.kr>
2025-06-22 15:07:45 +0900

    refac: import시 와일드카드 축약 제거

--- core/src/main/kotlin/siksha/wafflestudio/core/domain/main/menu/data/Menu.kt
+++ core/src/main/kotlin/siksha/wafflestudio/core/domain/main/menu/data/Menu.kt
@@ -1,6 +1,11 @@
 package siksha.wafflestudio.core.domain.main.menu.data
 
-import jakarta.persistence.*
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+import jakarta.persistence.Table
 import org.hibernate.annotations.CreationTimestamp
 import org.hibernate.annotations.UpdateTimestamp
 import java.time.LocalDate
