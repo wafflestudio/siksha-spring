9bfd4be7c19cc15f74b082a10e39b48456c87a0e
dennis0405 <dennis0405@snu.ac.kr>
2025-07-06 14:29:18 +0900

    [Menu API Controller 로그인/비로그인 logic 통합]

--- api/src/main/kotlin/siksha/wafflestudio/api/common/AuthInterceptor.kt
+++ api/src/main/kotlin/siksha/wafflestudio/api/common/AuthInterceptor.kt
@@ -18,6 +18,7 @@ class AuthInterceptor(
     @Value("\${jwt.secret-key}") private val jwtSecretKey: String,
 ) : HandlerInterceptor {
     private val logger = LoggerFactory.getLogger(javaClass)
+    private val menuGetPattern = Regex("^/menus(?:/\\d+)?$")  // /menus 또는 /menus/{menu_id}
     private lateinit var encodedJwtSecretKey: SecretKey
 
     init {
@@ -35,8 +36,21 @@ class AuthInterceptor(
         if (request.method == HttpMethod.GET.name() && request.requestURI == "/community/boards") return true
         if (request.method == HttpMethod.OPTIONS.name()) return true
 
+        // /menus/와 /menus/lo api를 /menus로 통합하기 위한 처리
+        val authHeader = request.getHeader(AUTHORIZATION_HEADER_NAME)
+        if (request.method == HttpMethod.GET.name() && menuGetPattern.matches(request.requestURI)) {
+            // 로그인하지 않은 상태로 /menus, /menus/{menu_id} 요청 시, userId를 0으로 설정
+            // exception 발생 하지 않음
+            if (authHeader.isNullOrBlank()) {
+                request.setAttribute("userId", 0)
+                return true
+            }
+        }
+
+        // 로그인 상태에서 /menus, /menus/{menu_id} 요청 시, userId를 request attribute에 설정
+        // 해당 과정에서 token이 잘못된 경우, UnauthorizedUserException 발생
+        // 다른 api에 대해서도 정상적으로 token 검증 작업
         return runCatching {
-            val authHeader = request.getHeader(AUTHORIZATION_HEADER_NAME)
             val userId = verifyJwtGetClaims(authHeader)[USER_ID_KEY_IN_JWT]!! as Int
             request.setAttribute("userId", userId)
             true
