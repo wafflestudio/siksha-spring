a234c9974de9716f8d15912aed807226b0b7a686
Junyong Lee <leejunyong02@snu.ac.kr>
2025-07-09 17:39:00 +0900

    [Exception] MainException.kt 추가

---
+++ core/src/main/kotlin/siksha/wafflestudio/core/domain/common/exception/MainException.kt
@@ -0,0 +1,12 @@
+package siksha.wafflestudio.core.domain.common.exception
+
+import org.springframework.http.HttpStatus
+
+sealed class MainException(httpStatus: HttpStatus, errorMessage: String) : SikshaException(httpStatus, errorMessage)
+
+class RestaurantNotFound : MainException(HttpStatus.NOT_FOUND, "Restaurant not found")
+
+class InvalidScoreException : MainException(HttpStatus.BAD_REQUEST, "score는 1에서 5 사이여야 합니다.")
+
+class MenuNotFoundException: MainException(HttpStatus.NOT_FOUND, "해당 메뉴를 찾을 수 없습니다.")
+
