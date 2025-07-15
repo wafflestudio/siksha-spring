89d0567296541a3d001c3635e42c0bb8f9f5e98a
Junyong Lee <97828079+alpakar02@users.noreply.github.com>
2025-07-15 15:58:28 +0900

    feat: 리뷰 API 7개 (#42)
    * [Directory changes] Review -> Main / [Review Repository] function added
    
    * [Menu.kt] restaurant에 있던 주석 제거
    
    * [Review DTO] Review Request/Response Dtos
    
    * [Exception] MainException.kt 추가
    
    * [Review Controller/Service] 리뷰 API 7개 이전
    
    리뷰 API 7개 이전 (FastAPI -> Spring)
    
    POST /reviews/images
    POST /reviews/
    GET /reviews/
    GET /reviews/comments/recommendation
    GET /reviews/dist
    GET /reviews/filter
    GET /reviews/me

--- core/src/main/kotlin/siksha/wafflestudio/core/domain/review/repository/ReviewRepository.kt
+++
@@ -1,8 +0,0 @@
-package siksha.wafflestudio.core.domain.review.repository
-
-import org.springframework.data.jpa.repository.JpaRepository
-import org.springframework.stereotype.Repository
-import siksha.wafflestudio.core.domain.review.data.Review
-
-@Repository
-interface ReviewRepository : JpaRepository<Review, Int>
