a7031b3125a88364df56393a9b07ae910d1746f2
Junyong Lee <leejunyong02@snu.ac.kr>
2025-09-21 13:16:19 +0900

    리뷰 관련 예외 추가
    ReviewNotFoundException: 해당 리뷰가 존재하지 않는 경우
    NotReviewOwnerException: 해당 리뷰를 작성한 사용자가 아닌 경우

--- core/src/main/kotlin/siksha/wafflestudio/core/domain/common/exception/MainException.kt
+++ core/src/main/kotlin/siksha/wafflestudio/core/domain/common/exception/MainException.kt
@@ -31,3 +31,7 @@ class ReviewAlreadyExistsException : MainException(HttpStatus.CONFLICT, "이 메
 class ReviewSaveFailedException : MainException(HttpStatus.INTERNAL_SERVER_ERROR, "리뷰 저장 중에 오류가 발생했습니다.")
 
 class SelfReviewLikeNotAllowedException : MainException(HttpStatus.BAD_REQUEST, "본인의 리뷰에는 좋아요를 누를 수 없습니다.")
+
+class ReviewNotFoundException : MainException(HttpStatus.NOT_FOUND, "해당 리뷰가 존재하지 않습니다.")
+
+class NotReviewOwnerException : MainException(HttpStatus.NOT_FOUND, "해당 리뷰를 작성한 사용자가 아닙니다.")
