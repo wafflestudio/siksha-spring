cfc2789308b141b7738c190d48cdb9beb0d639d4
정지민 <jungjimin424@naver.com>
2025-09-22 20:38:05 +0900

    fix: add Authorization header in swagger

--- api/src/main/kotlin/siksha/wafflestudio/api/common/SwaggerConfig.kt
+++ api/src/main/kotlin/siksha/wafflestudio/api/common/SwaggerConfig.kt
@@ -1,15 +1,35 @@
 package siksha.wafflestudio.api.common
 
+import io.swagger.v3.oas.models.Components
 import io.swagger.v3.oas.models.OpenAPI
 import io.swagger.v3.oas.models.info.Info
+import io.swagger.v3.oas.models.media.StringSchema
+import io.swagger.v3.oas.models.parameters.Parameter
+import io.swagger.v3.oas.models.security.SecurityRequirement
+import io.swagger.v3.oas.models.security.SecurityScheme
+import org.springdoc.core.customizers.OpenApiCustomizer
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Profile
 
 @Configuration
+@Profile("local", "dev")
 class SwaggerConfig {
     @Bean
     fun openApi(): OpenAPI {
         return OpenAPI()
+            .components(
+                Components().addSecuritySchemes(
+                    "bearerAuth",
+                    SecurityScheme()
+                        .type(SecurityScheme.Type.APIKEY)
+                        .`in`(SecurityScheme.In.HEADER)
+                        .name("Authorization")
+                )
+            )
+            .addSecurityItem(
+                SecurityRequirement().addList("bearerAuth")
+            )
             .info(configurationInfo())
     }
 
