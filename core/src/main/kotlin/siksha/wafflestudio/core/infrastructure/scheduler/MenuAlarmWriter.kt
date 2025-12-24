09f455d637a7c6def55cb217efa2a9b97f867035
soobin-jeon <nname@snu.ac.kr>
2025-12-24 23:32:42 +0900

    lint

--- core/src/main/kotlin/siksha/wafflestudio/core/infrastructure/scheduler/MenuAlarmWriter.kt
+++ core/src/main/kotlin/siksha/wafflestudio/core/infrastructure/scheduler/MenuAlarmWriter.kt
@@ -31,7 +31,9 @@ class MenuAlarmWriter(
             val pushMessage =
                 PushMessage(
                     title = "\uD83D\uDD14띵동! 내가 찜한 메뉴가 나왔어요!",
-                    body = "오늘은 \uD83C\uDF7D\uFE0F${menuNames.joinToString(", ")}\uD83C\uDF7D\uFE0F${subjectParticle(menuNames.last())} 나오는 날이에요.\n알림을 클릭해 새로운 리뷰를 확인해 보세요.",
+                    body = "오늘은 \uD83C\uDF7D\uFE0F${menuNames.joinToString(
+                        ", ",
+                    )}\uD83C\uDF7D\uFE0F${subjectParticle(menuNames.last())} 나오는 날이에요.\n알림을 클릭해 새로운 리뷰를 확인해 보세요.",
                 )
 
             fcmPushClient.sendPushMessages(
@@ -44,7 +46,10 @@ class MenuAlarmWriter(
     private fun subjectParticle(word: String): String {
         val lastChar = word.last()
 
-        return if ((lastChar.code - '가'.code) % 28 != 0) "이"
-        else "가"
+        return if ((lastChar.code - '가'.code) % 28 != 0) {
+            "이"
+        } else {
+            "가"
+        }
     }
 }
