6a3c99c75d0bef9ce733023e375f3b4ccc2c7e9d
JunwooPark <76034729+kodugi@users.noreply.github.com>
2026-04-27 19:32:18 +0900

    불필요한 data, repository 삭제

--- core/src/main/kotlin/siksha/wafflestudio/core/domain/main/restaurant/repository/RestaurantOrderRepository.kt
+++
@@ -1,8 +0,0 @@
-package siksha.wafflestudio.core.domain.main.restaurant.repository
-
-import org.springframework.data.jpa.repository.JpaRepository
-import siksha.wafflestudio.core.domain.main.restaurant.data.RestaurantOrder
-
-interface RestaurantOrderRepository : JpaRepository<RestaurantOrder, Int> {
-    fun findRestaurantOrderByUserId(userId: Int): RestaurantOrder?
-}
