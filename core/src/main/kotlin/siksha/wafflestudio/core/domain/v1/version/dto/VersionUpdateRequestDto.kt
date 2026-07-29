8afa6b2df06fd4a11c49871520882656946a8d63
Kim Minsoo <144578175+dennis0405@users.noreply.github.com>
2026-07-29 11:26:11 +0900

    fix: version API 요청 예시를 snake_case로 표시 (#146)

--- core/src/main/kotlin/siksha/wafflestudio/core/domain/v1/version/dto/VersionUpdateRequestDto.kt
+++ core/src/main/kotlin/siksha/wafflestudio/core/domain/v1/version/dto/VersionUpdateRequestDto.kt
@@ -1,8 +1,11 @@
 package siksha.wafflestudio.core.domain.v1.version.dto
 
+import com.fasterxml.jackson.databind.PropertyNamingStrategies
+import com.fasterxml.jackson.databind.annotation.JsonNaming
 import jakarta.validation.constraints.Pattern
 import jakarta.validation.constraints.Size
 
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy::class)
 data class VersionUpdateRequestDto(
     @field:Pattern(
         regexp = "^[0-9]+\\.[0-9]+\\.[0-9]+$",
